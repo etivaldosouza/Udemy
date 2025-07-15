83a03e18c6e2bfb45abfd2557feec2809ccaaa4b
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-15 14:53:08 -0300

    strings especiais

---
+++ strings/scripts/scripts.js
@@ -0,0 +1,20 @@
+/*  Strings: 
+
+   - a \ pode da um escape na string, e isso permite efeitos especiais;
+   - por exemplo: \n pula uma linha
+   - o temlplate literal(``) é usado quando queremos utilizar variáveis no meio de uma string
+   - concatenar é um processo de somar strings ex: console.log("salada" + "de" + "frutas") 
+
+*/
+
+console.log('Essa é uma string \nde duas linhas')
+
+
+// o template literal(uso de aspas) serve também para computar valores, ex:
+console.log(`a soma de 2+2 é ${2+2}`)
+console.log(`a multiplicação de 5 por 2 é igual a ${5*2}`)
+
+// para inserir uma aspas simples(') ou aspas duplas(") devemos usar na string a aspas inversa  do console log ex:
+
+console.log('o ser humano é igual a massa de pão: só cresce "apanhando"')
+console.log("o ser humano é igual a massa de pão: só cresce 'apanhando'")
