db7e0bb390e8f44d8261e33599ea2ede27f87409
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-15 14:05:36 -0300

    tipos de dados

---
+++ tipos de dados/scripts.js
@@ -0,0 +1,20 @@
+// Number são tipo de dados que são números
+
+console.log(typeof 13)
+console.log(typeof 1.3)
+console.log(typeof -25)
+console.log(typeof 0)
+
+/*
+    ''special Number'':
+
+    temos tb os tipos de dados que são Number mas que não são numeros:
+*/
+
+console.log(Infinity)
+console.log(-Infinity)
+console.log(NaN)
+
+console.log(typeof Infinity) // o tipo de dado é Number porém ñ é um número
+console.log(typeof -Infinity)// o tipo de dado é Number porém ñ é um número
+console.log(typeof NaN)      // o tipo de dado é Number porém ñ é um número
