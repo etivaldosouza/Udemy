875e09052157a353e40204d478674821e30b299c
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-23 15:13:17 -0300

    Update scripts.js

--- secao_4/funcao/matth.x()/scripts/scripts.js
+++ secao_4/funcao/matth.x()/scripts/scripts.js
@@ -1,5 +1,5 @@
 /*
-        Math.x()
+        Math.x() onde x é o método que a gente quer
 
 => api que simula várias funções matemática
     ex: let maiorNumero = Math.max(1,5,2,3)
@@ -7,7 +7,15 @@
 
 => função bastante utilizada
 
+=> nativa da linguagem
+
 */ 
 
 let maiorNumero = Math.max(1,5,2,3)
-    console.log(maiorNumero)
+console.log(maiorNumero)
+
+let arredondar = Math.round(8.2354)
+console.log(arredondar)
+
+let arredondarParaCima = Math.ceil(8.2354)
+console.log(arredondarParaCima)
