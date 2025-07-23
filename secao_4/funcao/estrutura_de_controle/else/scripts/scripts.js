77a52ea57c66b80fe1dd2e327143a2cbcca57cb8
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-23 15:59:00 -0300

    adicionado else

---
+++ secao_4/funcao/estrutura_de_controle/else/scripts/scripts.js
@@ -0,0 +1,18 @@
+/*
+            else
+
+=> podemos executar um outro bloco de código, caso a instrução do if não seja atendida
+
+=> ou seja, se não executar o bloco if, execute o bloco else
+
+=> só pode ser utilizada com o if porém o if pode ser usado sem o else
+
+*/
+
+let nome = 'Pedro'
+
+if(nome == 'joão') {
+console.log(`olá ${nome}`)
+}else {
+    console.log(`olá ${nome}`)
+}
