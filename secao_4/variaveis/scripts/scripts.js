a75014dcfdb3240c0731a66f1be8f526550754b9
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-22 21:17:33 -0300

    adicionado comentarios

---
+++ secao_4/variaveis/scripts/scripts.js
@@ -0,0 +1,39 @@
+/*
+        VARIÁVEIS
+
+    var => forma antiga optar por usar o let ou const
+
+    let => palavra reservada e atual de declarar uma variável
+
+    const => usado para quando não queremos mudar o valor da variável até o fim do programa.
+
+
+*/
+
+
+var nome = 'Etivaldo'
+const ip = '127.0.0.1'
+
+console.log(nome)
+console.log(ip)
+
+ip = 'jksdhjjshj' // irá dá erro pois a declaração inicial da variável foi utilizada com const, oq ñ permite alterar o valor até o fim do programa
+
+
+/*
+            CONVENÇÃO NO NOME DA VARIÁVEL:
+
+    => Ñ PODE COMEÇAR COM NÚMERO. ex:  let 2test
+
+    => PODE TERMINAR COM NÚMERO. ex: let teste2
+
+    => PODE TER $ OU _ MAS OUTROS CARCTERES NÃO. ex: let $nome e _nome;
+
+    => PODEMOS INICIAR COM LETRAS MAIÚSCULA ex: 
+    let MeuPrimeiroNome = 'matheus' (PascalCase)
+
+    => PODEMOS USAR camelCase. 
+    ex: let MeuPrimeiroNome = 'João'
+*/
+
+
