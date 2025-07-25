6dbfd83446f739d79c47a606c9403a23b8b36188
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-25 15:20:12 -0300

    estrutura de repetição

---
+++ secao_4/funcao/estrutura_de_repeticao/scripts/scripts.js
@@ -0,0 +1,13 @@
+/*
+             o que são estruturas de repetição
+
+    => São estruturas que permitem repetir um bloco de código várias vezes, dependendo de uma condição.
+
+    => a idéia é repetir uma ação até atingir uma condição.
+
+    => ao invés de repetirmos o mesmo código várias vezes, criamos um statement que fará uma checagem em cada loop sendo verdadira será repetido a acao novamente sendo falsa o loop é encerrado.
+
+    => é também chamada de loop;( escrever uma linha de código e repetir ela várias vezes)
+
+*/ 
+
