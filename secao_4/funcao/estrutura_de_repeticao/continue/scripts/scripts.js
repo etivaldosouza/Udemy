10719e0dc7259ac2afbe123e0b8e9b5c2c797c0f
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-28 11:04:11 -0300

    continue

---
+++ secao_4/funcao/estrutura_de_repeticao/continue/scripts/scripts.js
@@ -0,0 +1,9 @@
+/*
+    Pulando uma execução do loop: (continue)
+
+
+=> dependendo da nossa lógica podemos pular o resto da execução do loop
+
+=> para isso utilizamos a palavra reservada continue
+
+*/ 
