6f09ade2654cdfda5595910cd7336c0716f21feb
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-07-31 13:58:32 -0300

    switch

---
+++ secao_4/funcao/estrutura_de_repeticao/swith/scripts/scripts.js
@@ -0,0 +1,28 @@
+/*
+    estrutura condicional switch:
+
+=>  quando há a necessidade de vários ifs, podemos utilizar o switch/case
+
+=> para sair de um case podemos utlizar o break
+
+=> podemos inserir uma expressão default, para caso nenhum valor for correspondido
+
+=> muitos programadores optam por usar vários if;
+
+*/
+
+
+/*
+    dias da semana em JS:
+
+    0: domingo
+    1: segunda
+    2: terça
+    3: quarta
+    4: quinta
+    5: sexta
+    6: sábado
+    
+*/ 
+
+
