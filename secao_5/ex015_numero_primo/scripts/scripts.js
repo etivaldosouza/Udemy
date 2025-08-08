757f31360c0dde038b475c855e9fbe81db2cffb0
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-08 15:57:02 -0300

    Update scripts.js

--- secao_5/ex015_numero_primo/scripts/scripts.js
+++ secao_5/ex015_numero_primo/scripts/scripts.js
@@ -8,19 +8,26 @@
 // na estrutura while e for tem q ter o incrementro/decremento p/ ñ cair no loop infinito
 
 
-let num = 3 // nr que sera testado se primo ou ñ
-let divisoes = 0 // contador de divisoes
+let num = 5 // nr que será testado se primo ou ñ
+let divisoes = 0 // contador dos divisores
 
-
-for(let i = 1; i <= num; i++){
+for(let i = 1; i <= num; i++){ 
     if(num % i == 0){
-        divisoes++
-    } 
+        divisoes++                    
+    }  
 }
+
 
 if(divisoes == 2){
-    console.log(`o numero é primo`)
+    console.log(`o numero ${num} é primo`)
+    
 }else{
-    console.log(`o número ñ é primo`)
+    console.log(`o número ${num} ñ é primo`) // tem mais de 2 divisores
 }
+
+// botei o i p/ começar no 1 pq começando com 0 ñ daria certo pq no primeiro caso cairia em 0/0 => e ñ existe divisão por zero
+
+// em seguida o codigo continua p/ saber se é primo ou nao
+
+
 
