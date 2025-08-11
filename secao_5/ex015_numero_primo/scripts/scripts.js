9651626cd920b705de88429fcb69987ef2a2be23
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-11 08:33:50 -0300

    Update scripts.js

--- secao_5/ex015_numero_primo/scripts/scripts.js
+++ secao_5/ex015_numero_primo/scripts/scripts.js
@@ -1,22 +1,22 @@
 /*
     verifique se o número é primo!
 
-    - número primo, é um número natural maior que 1 que é divisivel por um e por ele mesmo
+    - número primo é um número natural maior que 1 que é divisivel por um e por ele mesmo,ou seja o número para ser primo tem que ter apenas 2 divisores o 1 e ele mesmo.
 
 */ 
 
 // na estrutura while e for tem q ter o incrementro/decremento p/ ñ cair no loop infinito
 
 
-let num = 5 // nr que será testado se primo ou ñ
+let num = 4 // nr que será testado se primo ou ñ
 let divisoes = 0 // contador dos divisores
 
 for(let i = 1; i <= num; i++){ 
+
     if(num % i == 0){
         divisoes++                    
     }  
 }
-
 
 if(divisoes == 2){
     console.log(`o numero ${num} é primo`)
