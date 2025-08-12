725f494fcf2d091c9a06f4042fd94765eef69ee0
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-12 15:47:54 -0300

    Create script.js

---
+++ seção_6/funções/mais_sobre_funções/script.js
@@ -0,0 +1,16 @@
+const soma = function(a,b){
+    return a + b   
+}
+
+const total = soma(3,5)
+
+console.log(total) 
+
+
+// outra maneira:
+
+const soma = function(a,b){
+    return a + b   
+}
+
+console.log(soma(4,5))
