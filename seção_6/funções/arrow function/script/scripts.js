fb62ebe588efe36f4cbb6baaa19710ca271bf1ab
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-19 08:58:34 -0300

    arrow function

---
+++ seção_6/funções/arrow function/script/scripts.js
@@ -0,0 +1,16 @@
+/*
+    Arrow Function
+
+=> uma outra forma de declarar funções
+
+=> bem utilizadas nos frameworks modernos
+
+=> porém não deve substituir as functions por completo(veremos mais tarde os detalhes)
+
+*/
+
+const parOuImpar = (n) =>  {
+    return n % 2 ;
+}
+
+console.log(parOuImpar(3))
