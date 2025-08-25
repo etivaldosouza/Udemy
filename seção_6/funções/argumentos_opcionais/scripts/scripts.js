d72a4c0ee9bc4a3f5977a68fca1a48d8f9516d95
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-25 17:09:54 -0300

    Update scripts.js

--- seção_6/funções/argumentos_opcionais/scripts/scripts.js
+++ seção_6/funções/argumentos_opcionais/scripts/scripts.js
@@ -21,5 +21,25 @@ nomeComIdade('joão',42)
 
 
 function soma(a,b){
-    if(a === undefined)
+    if(a === undefined || b === undefined){
+        console.log(`esta função necessita dos dois valores`)
+    }else{
+        return a + b
+    }
 }
+
+console.log(soma(1))
+
+console.log(soma(2,5))
+
+// obs: Se você quiser que a função não mostre undefined quando faltar argumento, pode retornar algo no if, por exemplo:
+
+function soma(a,b){
+    if(a === undefined || b === undefined){
+        return "Esta função necessita dos dois valores"
+    }
+    return a + b
+}
+
+console.log(soma(1))    
+console.log(soma(2,5))  
