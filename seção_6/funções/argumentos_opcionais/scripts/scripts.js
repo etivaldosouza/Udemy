244cbb0804c23c7b1da7b5e97b2ff7079935ccfe
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-25 16:54:30 -0300

    Update scripts.js

--- seção_6/funções/argumentos_opcionais/scripts/scripts.js
+++ seção_6/funções/argumentos_opcionais/scripts/scripts.js
@@ -4,5 +4,22 @@
 
 => Podemos chamar uma função em JS sem o número igual de parâmetros determinados
 
-
 */ 
+
+function nomeComIdade (nome,idade){
+
+    if(idade === undefined){
+        console.log(`seu nome é ${nome}`)
+    }else{
+        console.log(`seu nome é ${nome} e você tem ${idade} anos`)
+    }
+}
+
+nomeComIdade('João')
+
+nomeComIdade('joão',42)
+
+
+function soma(a,b){
+    if(a === undefined)
+}
