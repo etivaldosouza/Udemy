01e55c768ca8a587f8fd870a6effeb64ea2fb574
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-08-31 09:51:05 -0300

    Update scripts.js

--- seção_6/funções/exercicios_funçoes/ex_04_numero_aleatorio/scripts/scripts.js
+++ seção_6/funções/exercicios_funçoes/ex_04_numero_aleatorio/scripts/scripts.js
@@ -6,8 +6,23 @@
 */ 
 
 function sorteio(x){
-    return Math.random(x)  * 10
+    let sort = Math.random() * x//sorteia um numero entre 0 e x
+    return Math.round(sort) // arredonda o número
 
 }
 
+console.log(sorteio(100)) // utilizando parâmetro
 console.log(sorteio(10))
+console.log(sorteio(5))
+
+
+
+//===================/ / SEM PARÂMETRO / /======================================//
+
+function sorteio(){
+    let sort = Math.random()  * 5 //sorteia um numero entre 0 e 5
+    return Math.round(sort) // arredonda o número
+
+}
+
+console.log(sorteio())
