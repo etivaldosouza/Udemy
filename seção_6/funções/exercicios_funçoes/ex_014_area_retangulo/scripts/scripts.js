711ea45728362dda3d2ab255393f1814eae4c66e
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-02 09:54:18 -0300

    criado ex014

---
+++ seção_6/funções/exercicios_funçoes/ex_014_area_retangulo/scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    Área do retângulo
+
+=> Crie uma função chamada areaRetangulo que recebe largura e altura
+
+=> Retorne a área (largura * altura).
+
+=> Crie uma variável resultado que receba o retorno da função passando 5 e 12.
+
+=> Mostre o resultado no console.
+
+*/ 
