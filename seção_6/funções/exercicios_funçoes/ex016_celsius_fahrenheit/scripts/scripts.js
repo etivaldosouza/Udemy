9700aa6891f1b232aa5b4d0ea60aba2f17be4323
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-02 10:12:14 -0300

    adicionado exercicio 16

---
+++ seção_6/funções/exercicios_funçoes/ex016_celsius_fahrenheit/scripts/scripts.js
@@ -0,0 +1,23 @@
+/*
+    celsis para Fahrenheit
+
+=> Crie uma função chamada celsiusParaFahrenheit que recebe uma temperatura em graus Celsius.
+
+=> Retorne o valor convertido para Fahrenheit usando a fórmula:
+
+    F = (C * 9/5) + 32
+
+
+=> Crie uma variável resultado que receba o retorno da função passando 30 como argumento.
+
+=> Mostre o resultado no console.
+
+*/ 
+
+function celsiusParaFahrenheit (c){
+    return F = (c * 9/5) + 32
+}
+
+let resultado = celsiusParaFahrenheit(30)
+
+console.log(resultado)
