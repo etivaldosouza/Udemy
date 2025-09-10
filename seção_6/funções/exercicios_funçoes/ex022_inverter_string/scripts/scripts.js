64be11ac744bd4ef95138fc24315574c43c1ced0
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-10 07:47:51 -0300

    criado exercicio22

---
+++ seção_6/funções/exercicios_funçoes/ex022_inverter_string/scripts/scripts.js
@@ -0,0 +1,10 @@
+/*
+Escreva uma função chamada reverseString que recebe uma string como argumento.
+
+A função deve inverter a ordem dos caracteres na string.
+
+A função deve retornar a string invertida.
+
+*/ 
+
+fun
