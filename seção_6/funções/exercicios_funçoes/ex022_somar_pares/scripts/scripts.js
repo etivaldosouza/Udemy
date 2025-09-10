befc4007f1ab2b59303047eaa7a971d861d4be65
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-10 08:37:27 -0300

    Update scripts.js

--- seção_6/funções/exercicios_funçoes/ex022_somar_pares/scripts/scripts.js
+++ seção_6/funções/exercicios_funçoes/ex022_somar_pares/scripts/scripts.js
@@ -1,10 +1,33 @@
 /*
-Escreva uma função chamada reverseString que recebe uma string como argumento.
+    Somando os Números Pares
 
-A função deve inverter a ordem dos caracteres na string.
+=>  Escreva uma função chamada sumEvenNumbers que recebe um array de números como argumento.
 
-A função deve retornar a string invertida.
+=>  A função deve calcular e retornar a soma de todos os números pares presentes no array.
+
+=>  Utilize o operador de módulo (%) para determinar se um número é par.
+
+=>  Considere que o array pode conter tanto números inteiros positivos quanto negativos.
+
+=>  Dica: Para verificar se um número é par, você pode usar a expressão numero % 2 === 0, onde % é o operador de módulo que retorna o resto da divisão.
 
 */ 
+arr = [2,3,4,5,8,9]
 
-fun
+
+function sumEvenNumbers(arr){  
+
+    let soma = 0
+
+    for(let i = 0; i < arr.length; i++){
+
+        if(arr[i] % 2 === 0){
+            soma = soma + arr[i]
+        }       
+    }
+    
+    return soma        
+}
+
+let resultado = sumEvenNumbers([2,3,4,5,8,9])
+console.log(resultado)
