dbeca3070a8940df1e7a9d0d9cec5f97ba3f7ece
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-11 10:32:35 -0300

    ok

---
+++ seção_8_objetos_e_array/array/conceito/scripts/scripts.js
@@ -0,0 +1,43 @@
+/*
+            Arrays (conjuntos)
+
+=> array é um objeto JavaScript destinado a armazenar dados de qualque tipo, sequencialmente indexados a partir do zero.
+
+=> possibilidade de adicionar um conjunto de valores a uma variável
+
+=> o Array deve ser escrito entre colchetes, separando os valores por vírgulas, veja:
+
+*/ 
+
+// estrutura de um array:
+
+let numeros = [1,3,5,8,12]
+
+let informacoes = ["Matheus",12,true,"Teste",2]
+
+//============================================================//
+
+
+const arr = []  // criei um array vazio
+
+// criando elementos p/ o array vazio:
+
+arr[0] = 'Etivaldo' 
+arr[1] = 'Pedro'
+arr[2] = 'João'
+arr[3] = {profissão: 'Desenvolvedor', linguagem: 'JavaScript'}
+
+console.log(arr[3].linguagem) // imprimo o valor de linguagem no elemento 3 do array
+
+
+//==============================================================//
+
+//posso ter vários tipos de dados no array
+
+let arr = [1,true,5,false,'João']
+
+console.log(arr[0])
+
+console.log(arr[1])
+
+console.log(arr)
---
+++ seção_8_objetos_e_array/array/índice/scripts/scripts.js
@@ -0,0 +1,26 @@
+/*
+    Mais sobre arrays
+
+=> Podemos acessar um array pelo seu índice, por exemplo: arr[1]
+
+=> lembrando que o primeiro índice é o 0, ou seja, para acessar o primeiro elemento: arr[0]
+
+
+*/ 
+
+
+let arr = [1,4,5,6,7]
+
+let nomes = ['Matheus','João','Aléxia']
+
+let bool = [true,false,true]
+
+console.log(arr[1])
+
+console.log(nomes[0])
+
+console.log(bool[1])
+
+console.log(arr[arr.length - 1]) // para acessar o último elemento do array
+
+console.log(nomes[nomes.length - 1])
