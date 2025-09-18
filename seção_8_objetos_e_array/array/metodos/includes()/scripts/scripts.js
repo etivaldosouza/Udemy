f89775c4e8a5128c7bf7a1bf51286d80b22bc347
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-09-18 11:13:16 -0300

    adicionado metodo includes

---
+++ seção_8_objetos_e_array/array/metodos/includes()/scripts/scripts.js
@@ -0,0 +1,14 @@
+/*
+    includes()
+
+=> verifica se existe determinado elemento no array caso tenha retorna true caso contrário retorna false.
+
+*/ 
+
+
+let carros = ["BMW","Fiat","VW","Renault","Audi"]
+
+console.log(carros.includes("Fiat"))
+
+console.log(carros.includes("Ford"))
+
