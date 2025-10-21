d6d37bae1c4b8f6262b6e85dd292dfd579d4809b
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-10-20 21:43:35 -0300

    Update scripts.js

---
+++ seção_8_objetos_e_array/objetos/getters_setters/scripts/scripts.js
@@ -0,0 +1,30 @@
+/*
+    getters e setters
+
+*/ 
+
+var usuarios = [
+    {
+        nome: 'Ayrton',
+        idade: 26
+    },
+
+    {
+        nome: 'João',
+        idade: 19
+    },
+
+    {
+        nome: 'Maria',
+        idade: 21
+    }
+    
+]
+
+console.log(usuarios)
+
+console.log(usuarios[1])
+
+console.log(usuarios[1].nome)
+
+console.log(usuarios[2].nome)
