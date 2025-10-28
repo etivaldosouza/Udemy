b5cfac76eff93f1cc784aa6cde10e8e53078f74d
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-10-28 17:22:05 -0300

    Update scripts.js

--- seção_6_funções/funções/funcao_callback/scripts/scripts.js
+++ seção_6_funções/funções/funcao_callback/scripts/scripts.js
@@ -14,3 +14,36 @@ console.log(somaCallback(10,3,function(total){
     return total * 2
 }))
 
+
+//==================================================//
+
+
+function soma (a,b,CallBack){
+    return CallBack(a + b)
+}
+
+var resultado = soma(10,3,function(total){
+    return total * 3
+})
+
+console.log(resultado)
+
+
+//================ outra maneira ==========================//
+
+function soma(a,b,CallBack){
+    return CallBack(a + b)
+}
+
+var multiplica = function(resultado){
+    return resultado * 2
+}
+
+var total = soma(2,5,multiplica)
+console.log(total)
+
+//==========================
+
+console.log(soma(10,2,function(x){
+    return x + 20
+}))
