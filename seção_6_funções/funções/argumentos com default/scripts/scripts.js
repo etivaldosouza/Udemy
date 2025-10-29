9a0ef48806c7c4735da7b30ac97422e1fe4ad74b
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-10-29 16:33:04 -0300

    Update scripts.js

--- seção_6_funções/funções/argumentos com default/scripts/scripts.js
+++ seção_6_funções/funções/argumentos com default/scripts/scripts.js
@@ -23,6 +23,35 @@ function potencia(base, exp = 2){ //criei um valor default(exp = 2)
     return Math.pow(base,exp)
 }
 
-console.log(potencia(3)) // quando uso apenas um argumento eu irei pegar o valor default criado acima(exp = 2)
+console.log(potencia(3)) // quando uso apenas um argumento eu i
+// rei pegar o valor default criado acima(exp = 2)
 console.log(potencia(4,2)) // como os dois parâmetros foram passados usa-se eles.
 console.log(potencia(2,3))
+
+
+
+function repetirMSg(frase, n = 5){
+    for(let i = 1; i <= n; i++ ){
+        return `${frase} - ${i} ` 
+    }
+}
+
+console.log((repetirMSg('olá mundo')))
+
+
+
+
+function repetirMSg(frase, n = 5){
+
+    let resultado = ''
+
+    for(let i = 1; i <= n; i++){
+        resultado += `${frase} - ${i}\n`
+    }
+    return resultado
+}
+
+console.log(repetirMSg('olá mundo'))
+
+
+
