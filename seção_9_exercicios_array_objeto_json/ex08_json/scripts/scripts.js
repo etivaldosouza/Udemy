14fec256a80215c8720b4a08045e2ef0a5075e8a
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-10-31 16:47:46 -0300

    Update scripts.js

--- seção_9_exercicios_array_objeto_json/ex08_json/scripts/scripts.js
+++ seção_9_exercicios_array_objeto_json/ex08_json/scripts/scripts.js
@@ -2,9 +2,20 @@
 
 => crie um JSON com 3 propriedades
 
-=> Atribua ela a uma variável
+=> Atribua ele a uma variável
 
 => Acesse as propriedades imprimindo no console
 
 */ 
 
+
+let pm = {
+    "z" "Etivaldo",    "id": 822297,
+    "graduacao": "CB"
+}
+
+// p/ acessar as propriedades é igual ao objeto:
+
+let (nome,id,graduacao) = pm
+
+console.log(nome)
