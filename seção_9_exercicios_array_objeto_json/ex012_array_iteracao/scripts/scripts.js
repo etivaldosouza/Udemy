db5ef405d5d569f9c86bbf22fa59b4e9f2c8ec41
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-05 16:57:47 -0300

    Update scripts.js

--- seção_9_exercicios_array_objeto_json/ex012_array_iteracao/scripts/scripts.js
+++ seção_9_exercicios_array_objeto_json/ex012_array_iteracao/scripts/scripts.js
@@ -1,6 +1,19 @@
+/*
+
+=> Insira 5 elementos na variável arr;
+
+=> Os elementos devem ser um número, somando o iterador mais 5;
+
+=> O iterador é a variável temporária que controla o loop;
+
+*/ 
+
+
 const arr = []
 
 
-for(let i = 0; i < 5; i+5){
-    let nov = arr.
+for(let i = 0; i < 5; i++){
+    arr.push(i+5)    
 }
+
+console.log(arr)
