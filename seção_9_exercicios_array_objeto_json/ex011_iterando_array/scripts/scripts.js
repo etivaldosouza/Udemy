69e702423ec85ff690d0e64e55c4d1748e546fcf
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-05 20:27:30 -0300

    ok

--- seção_9_exercicios_array_objeto_json/ex011_iterando_array/scripts/scripts.js
+++ seção_9_exercicios_array_objeto_json/ex011_iterando_array/scripts/scripts.js
@@ -6,6 +6,19 @@
 
 */ 
 
+let arr = [1,2,3,4]
+
+let qty = arr.length
+
+let el = arr[2]
+
+console.log(el)
+
+
+
+//===========================================================//
+
+
 const arr = [1, 2, 3, 4]
 
 for(let i = 0; i < arr.length; i++){
--- seção_9_exercicios_array_objeto_json/ex012_preenchendo_array/scripts/scripts.js
+++ seção_9_exercicios_array_objeto_json/ex012_preenchendo_array/scripts/scripts.js
@@ -13,7 +13,9 @@ const arr = []
 
 
 for(let i = 0; i < 5; i++){
-    arr.push(i+5)    
+    console.log(i)
+    arr.push(i+5) 
+       
 }
 
 console.log(arr)
