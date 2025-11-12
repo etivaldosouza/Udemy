8ea657f7d7d6c59499abe501b6613f229019bcb9
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-12 20:12:45 -0300

    Update scripts.js

--- seção_4/variaveis/scripts/scripts.js
+++ seção_4/variaveis/scripts/scripts.js
@@ -36,4 +36,69 @@ ip = 'jksdhjjshj' // irá dá erro pois a declaração inicial da variável foi 
     ex: let MeuPrimeiroNome = 'João'
 */
 
+const nome = 'Souza'
 
+function qra(){
+    const nome = 'Etivaldo'
+    console.log(nome)
+}
+
+qra()
+
+nome = 'coelho' //(COM CONST Ñ PERMITE REDEFINIR)
+
+console.log(nome) // vai dá erro pois com const nao permite a gente nem redeclarar e nem redefinir:
+
+// const nome = coelho (redeclarar)
+// nome = coelho (reatribuir)
+
+
+// com let  permite apenas reatribuirmos(alterar a variavel)
+
+
+let idade = 25
+console.log(idade)
+
+idade = 30
+console.log(idade)
+
+
+// com var permirte tanto redeclarar como redefinir
+
+var idade = 10
+console.log(idade)
+
+var idade = 30
+console.log(idade)
+
+idade = 40
+console.log(idade)
+
+
+/*
+    particularidade da constante 
+
+=> um valor que pertence a uma constante pode ser alterado.
+ex:
+*/ 
+
+const linguagens = ['Java','Python','JS']
+
+linguagens[2] = 'GO'
+console.log(linguagens)  // alterei um valor dentro da constante
+
+
+const linguagens = ['html','css','rubi'] // vai dá erro pois alterei toda a constante
+console.log(linguagens)
+
+// exemplo 2:
+
+const usuario = {
+    nome: 'Etivaldo',
+    idade: 25
+}
+
+usuario.nome = 'Pedro'
+usuario.idade = '55'
+
+console.log(usuario)
