17867159e88e059e0a5ddaafee1fb1c506943cfa
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-18 20:14:45 -0300

    Update scripts.js

--- seção_10_orientação_objetos/prototype/scripts/scripts.js
+++ seção_10_orientação_objetos/prototype/scripts/scripts.js
@@ -12,7 +12,7 @@
 
 
 */ 
-/*
+
 let pessoa = {
     maos: 2
 }
@@ -21,9 +21,11 @@ console.log(Object.getPrototypeOf(pessoa))
 
 console.log(Object.getPrototypeOf(pessoa) === Object.prototype)
 
+console.log(pessoa.hasOwnProperty('maos'))
 
 
-*/
+
+
 
 
 function pessoa(nome){
