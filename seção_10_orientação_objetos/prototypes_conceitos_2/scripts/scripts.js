48284f5d08b6643913c5939c7789eab3b7340f10
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-21 15:31:01 -0300

    ok

---
+++ seção_10_orientação_objetos/prototypes_conceitos_2/scripts/scripts.js
@@ -0,0 +1,74 @@
+/*
+
+
+
+*/ 
+
+
+
+/*
+const meuObjeto = {
+    a: 1,
+    b: 2
+}
+    cadeia de prototype do objeto acima:
+
+    meuObjeto.__proto__ ==> Object.prototype ==> null
+
+*/ 
+
+
+/*
+const meuArray = [1,2,3, 'a','b']
+
+    cadeia de prototype do array acima
+    meuArrayl.__proto__ ==> Object.prototype ==> null
+*/ 
+
+
+
+
+/*
+function minhaFuncao(){
+    return 1 * 2
+}
+    cadeia de prototype da função acima
+    minhaFuncao.__proto__ ==> Object.prototype ==> null
+*/ 
+
+
+
+const meuObjeto = {
+    a: 1,
+    b: 2
+}
+
+const novoObjeto = Object.create(meuObjeto)
+
+novoObjeto.b = 6
+novoObjeto.novaPropriedade = 3
+
+console.log(meuObjeto)
+console.log(novoObjeto)
+
+console.log(meuObjeto.b + novoObjeto.b)
+
+console.log(meuObjeto.hasOwnProperty('map'))
+
+
+
+function salaAula(alunos){
+    this.alunos = alunos
+}
+
+salaAula.prototype = {
+    adicionarAluno: function(aluno){
+        this.alunos.push(aluno)
+    }
+}
+
+const minhaSala = new salaAula(['João','Maria'])
+
+minhaSala.adicionarAluno('pedro')
+
+console.log(minhaSala.alunos)
