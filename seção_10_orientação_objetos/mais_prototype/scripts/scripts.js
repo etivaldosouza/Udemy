2c57d171b394a6111f17b2c4dd7740be3f9070b2
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-21 16:05:32 -0300

    entendendo herança atraves de prototype

---
+++ seção_10_orientação_objetos/mais_prototype/scripts/scripts.js
@@ -0,0 +1,42 @@
+const pessoa = {
+    maos: 2
+}
+
+console.log(Object.getPrototypeOf(pessoa)) // p/ saber o prototype do objeto pessoa
+
+
+console.log(Object.getPrototypeOf(pessoa) === Object.prototype)
+
+
+// criando um novo objeto usando como referencia um objeto existente:
+
+const pessoa = {
+    maos: 2
+}
+
+const pessoaNova = Object.create(pessoa) 
+
+console.log(pessoaNova.maos) // está acessando(herdando) a propriedade maos do objeto herdado pessoa porém pessoaNova ñ tem a propriedade maos.
+
+console.log(pessoaNova.hasOwnProperty('maos')) // p/ saber se existe a propriedade maos no objeto pessoaNova
+
+console.log(Object.getPrototypeOf(pessoaNova) === pessoa) // quero saber se pessoa é prototype de pessoaNova
+
+
+/*
+    o que está acontecendo:
+
+=>  object.create(pessoa) cria um novo objeto vazio, mas com o protótipo apontando para o objeto pessoa.
+
+Ou seja:
+
+=>  pessoaNova não tem a propriedade maos própria,
+
+=>  mas ele herda maos do protótipo, que é o objeto pessoa.
+
+
+
+obs: mesmo eu nao criando o metodo hasOwnProperty para o pessoa ele pode ser utilizado pois o objeto pessoa ta herdando esse metodo do seu prototype Object. por tanto o objeto pessoa vai poder utilizar os mesmos métodos do Object.
+
+*/ 
+
