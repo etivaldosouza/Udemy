0c89cec767a82a46c756b0d9a20fb48be96ea48a
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-11-24 22:10:51 -0300

    adicionado mais exercicios sobre class

--- seção_10_orientação_objetos/classes_conceitos/scripts/scripts.js
+++ seção_10_orientação_objetos/classes_conceitos/scripts/scripts.js
@@ -22,10 +22,27 @@ pastorAlemao.raca = 'pastor Alemão'
 
 console.log(pastorAlemao.raca)
 
+
+//===============================================================================
+
+
+
+class Cachorro {
+    constructor(raca){
+        this.raca = 'SRD'
+    }
+    
+}
+
+let pastorAlemao = new Cachorro('Pastor Alemão')
+
+console.log(pastorAlemao.raca)
+
+
 //===================== outros exemplos =====================================
 
 
-let cachorro = {
+let cachorros = {
     patas: 4,
     raca: 'SRD',
     latir: function(){
@@ -33,7 +50,7 @@ let cachorro = {
     }
 }
 
-let labrador = Object.create(cachorro)
+let labrador = Object.create(cachorros)
 
 
 labrador.latir()
@@ -44,9 +61,44 @@ labrador.raca = 'Labrador'
 
 console.log(labrador.raca)
 
-console.log(cachorro.raca)
+console.log(cachorros.raca)
 
 
-pastor.raca = 'Pastor Alemão'
+let pastor = Object.create(cachorros)
+
+pastor.raca = 'Pastor Alemão' // estou incluindo a propriedade raça no objeto pastor
 
 console.log(pastor.raca)
+
+
+
+//===========================================================
+
+
+class Cachorro {
+    constructor(patas,raca){
+        this.patas = patas
+        this.raca = raca
+    }
+
+    latir(){
+        console.log('AuAuAu....')
+    }
+}
+
+
+let poodle = new Cachorro(4,'Poodle')
+
+let foxPaulistinha = new Cachorro(4,'Fox Paulistinha')
+
+let rotwailer = new Cachorro(4,'RottWeiler')
+
+
+console.log(poodle)
+poodle.latir()
+
+console.log(foxPaulistinha)
+poodle.latir()
+
+console.log(rotwailer)
+poodle.latir()
