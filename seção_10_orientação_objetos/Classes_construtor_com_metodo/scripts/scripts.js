46cb2d2b26108f4ec9a95084b870bf990a3adb30
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-01 16:35:11 -0300

    criado pasta construtor na classe

--- seção_10_orientação_objetos/Classes_construtor_com_metodo/scripts/scripts.js
+++ seção_10_orientação_objetos/Classes_construtor_com_metodo/scripts/scripts.js
@@ -7,3 +7,47 @@
 
 */
 
+
+function Cachorro(racaDoCachorro){
+
+    this.raca = racaDoCachorro
+
+}
+
+Cachorro.prototype.latir =  function(){
+
+    console.log('AuAuAu!!!')
+}
+
+let pug = new Cachorro('Pug')
+
+console.log(pug)
+
+pug.latir()
+
+
+
+//============================= mais exercicios ================================//
+
+
+function Cachorro(racaDoCachorro){
+
+    this.raca = racaDoCachorro
+
+}
+
+Cachorro.prototype.latir =  function(){
+
+    console.log('AuAuAu!!!')
+}
+
+Cachorro.prototype.uivar = function(){
+    console.log('AuuuUUU')
+}
+
+let pug = new Cachorro('Pug')
+
+console.log(pug)
+
+pug.latir()
+pug.uivar()
---
+++ seção_10_orientação_objetos/construtor_na_classe_ES6/scripts/scripts.js
@@ -0,0 +1,9 @@
+/*
+    Construtor na classe(ES6)
+
+=> Com a versão do ES6, uma possibilidade de criar uma classe(objeto) com construtor foi adicionada.
+
+=> entao não precisamos mais criar por meio de função
+
+
+*/ 
