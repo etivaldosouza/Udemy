0a3d8709336f0f50b61a1e6b332082647ba2a2a2
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-18 08:52:15 -0300

    Update scripts.js

--- secao_11_exercicios_O.O/ex02_carrinho_compras_2/scripts/scripts.js
+++ secao_11_exercicios_O.O/ex02_carrinho_compras_2/scripts/scripts.js
@@ -2,7 +2,80 @@
     Carrinho de Compras
 */ 
 
+  class Carrinho {
+    constructor(itens,qtd,valorTotal){
+      this.itens = itens
+      this.qtd = qtd
+      this.valorTotal = valorTotal
+    }
 
+    addItem(item){
+
+      let contador = 0 // para item q nao esta no carrinho
+      for(let itemCarrinho in this.itens){       //itemCarrinho é cada elemento do array(this.item)
+        if(this.itens[itemCarrinho].id === item.id){
+          this.itens[itemCarrinho].qtd += item.qtd
+          contador = 1
+        }
+      }
+      if(contador === 0){
+        this.itens.push(item)
+      }
+      this.qtd = this.qtd + item.qtd
+      this.valorTotal = this.valorTotal + item.qtd * item.preco
+    }
+    removeItem(item){
+      for(let itemCarrinho in this.itens){       //itemCarrinho é cada elemento do array(this.item)
+        if(this.itens[itemCarrinho].id === item.id){
+          
+          
+          let obj = this.itens[itemCarrinho]
+          let index = this.itens.findIndex(obj => obj.id === item.id)   // identificar o index do elemento a ser removido
+
+          this.qtd = this.qtd - this.itens[itemCarrinho].qtd 
+          this.valorTotal = this.valorTotal - this.itens[itemCarrinho].qtd * this.itens[itemCarrinho].preco
+
+          this.itens.splice(index,1)         
+        
+        }
+      }
+    }
+
+  }
+
+  let carrinho = new Carrinho([
+
+    {
+      id: 1,
+      nome: 'Camisa',
+      qtd: 1,
+      preco: 20
+    },
+
+    {
+      id: 2,
+      nome: 'Calça',
+      qtd: 2,
+      preco: 50
+    },
+  
+  ],3,120)
+
+  console.log(carrinho)
+
+
+  carrinho.addItem({id: 1, nome: 'Camisa', qtd: 2, preco: 20})
+
+
+  console.log(carrinho)
+
+  carrinho.addItem({id: 3, nome: 'Boné', qtd: 1, preco: 15})
+
+  console.log(carrinho)
+
+  carrinho.removeItem({id: 1, nome: 'Camisa', qtd: 2, preco: 20})
+
+  console.log(carrinho)
 /*
 
 obs:
