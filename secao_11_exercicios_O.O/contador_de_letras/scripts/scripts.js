fe616095dd2691fb4f9ffd13ebaff02ce0606f88
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-27 13:50:29 -0300

    criado exercicio contador de letras

---
+++ secao_11_exercicios_O.O/contador_de_letras/scripts/scripts.js
@@ -0,0 +1,14 @@
+/*
+    Contador de Letras
+
+=> Crie uma classe que conte quantas letras existem em uma frase, ignorando espaços
+
+=> espaços não contam
+
+=> acentos contam como letras normais
+
+=> números e simbolos não contam.
+
+*/ 
+
+
