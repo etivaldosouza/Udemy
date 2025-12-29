9625dacd95a282e16b3486f1c7f8cd17f4175ec0
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-29 13:56:14 -0300

    Update scripts.js

--- secao_11_exercicios_O.O/contador_de_letras/scripts/scripts.js
+++ secao_11_exercicios_O.O/contador_de_letras/scripts/scripts.js
@@ -16,6 +16,28 @@ class LetterCounter{
     constructor(){
 
     }
-}   countLetter(str){
-    let textoTratado = str.trim().
+    countLetter(str){
+        
+    let textoTratado = str.trim().replace(/\s+/g,"")
+
+    let letter = textoTratado.split('')
+
+    if(letter[0] === ''){
+        return 0
+    }
+    return letter.length
 }
+
+}
+
+   
+
+let counter = new LetterCounter()
+
+console.log(counter.countLetter('olá mundo!!'))
+
+
+let texto = 'Olá mundo!!'
+texto.trim().replace(/\s+/g,"")
+let arr = texto.split('')
+console.log(arr.length)
