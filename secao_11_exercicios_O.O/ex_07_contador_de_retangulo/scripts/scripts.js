9bed79f4de6f894d2916704aa6f7d2a55b44ffaa
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-29 16:13:08 -0300

    adicionado exercicio 07 calculadora de retamgulo

---
+++ secao_11_exercicios_O.O/ex_07_contador_de_retangulo/scripts/scripts.js
@@ -0,0 +1,41 @@
+/*
+    Calculadora de retângulo
+    
+=> Crie uma classe chamada Retângulo que represente um retangulo.
+
+1. A classe deve ter os seguintes atributos:
+
+    => largura: representa a largura do retângulo(número maior que zero)
+
+    => altura: representa a altura do retamgulo(número maior que zero)
+
+2. A classe deve ter os seguintes métodos:
+
+ calcularArea() : retorna a área do retangulo(largura * altura)
+
+ calcularPerimetro(): retorna o perímetro do retângulo(2 *(largura + altura))
+
+*/ 
+
+
+class Retamgulo{
+    constructor(largura,altura){
+        this.largura = largura
+        this.altura = altura
+    }
+    calcularArea(){
+        let area = this.largura * this.altura
+        return area
+
+    }
+    calcularPerimetro(){
+        let perimetro = 2 * (this.altura + this.largura)
+        return perimetro
+    }
+}
+
+
+let calculadoraRetangulo = new Retamgulo(3,2)
+
+console.log(calculadoraRetangulo.calcularArea())
+console.log(calculadoraRetangulo.calcularPerimetro())
