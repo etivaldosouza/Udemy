19d0f461b90096a9f863249d6a6439737278657b
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-12-29 15:00:16 -0300

    Update scripts.js

--- secao_11_exercicios_O.O/contador_de_letras/scripts/scripts.js
+++ secao_11_exercicios_O.O/contador_de_letras/scripts/scripts.js
@@ -18,15 +18,9 @@ class LetterCounter{
     }
     countLetter(str){
         
-    let textoTratado = str.trim().replace(/\s+/g,"")
+    let textoTratado = str.replace(/[^a-zà-ú]/gi,'')
 
-    let letter = textoTratado.split('')
-
-    if(letter.length === 0){
-        return 0
-    }
-    else if(letter )
-    return letter.length
+    return textoTratado.length
 }
 
 }
@@ -35,15 +29,120 @@ class LetterCounter{
 
 let counter = new LetterCounter()
 
-console.log(counter.countLetter('olá mundo!!!'))
+console.log(counter.countLetter('olá234mundo! 12345'))
+
+
+
+/*
+    .replace(/[^a-zà-ú]/gi, '')
+
+    Essa linha significa:
+
+=> “remova tudo que NÃO for letra”
+
+
+O que é / ... /
+
+Em JavaScript, tudo entre barras é uma expressão regular (regex).
+
+/alguma-coisa/
+
+
+O papel do ^ (acento circunflexo)
+[^ ... ]
+
+
+Dentro de colchetes [], o ^ inverte o sentido.
+
+[abc] → aceita a ou b ou c
+
+[^abc] → aceita tudo EXCETO a, b ou c
+
+👉 Ou seja: negação
+
+
+[a-z] → letras minúsculas
+[a-z]
+
+
+Representa todas as letras de a até z.
+
+
+à-ú → letras acentuadas
+[a-zà-ú]
+
+
+Inclui letras como:
+
+á à â ã é ê í ó ô õ ú ç
+
+
+Isso é importante para português 🇧🇷.
 
 
 
 
 
+Juntando tudo dentro dos colchetes
+[^a-zà-ú]
 
 
-let texto = ''
-let tratarTexto = texto.split('')
-console.log(tratarTexto)
-console.log(tratarTexto.length)
+Significa:
+
+❌ qualquer caractere que NÃO seja
+✔️ letra de a a z
+✔️ letra acentuada de à a ú
+
+Ou seja, isso pega:
+
+espaços
+
+números
+
+pontuação (! , . ?)
+
+símbolos (@ # $)
+
+
+O g (global)
+/g
+
+
+Sem ele, o replace removeria só o primeiro caractere encontrado.
+
+Com g:
+👉 remove todos.
+
+
+O i (ignore case)
+/i
+
+
+Faz o regex ignorar diferença entre:
+
+maiúsculas
+
+minúsculas
+
+Então:
+
+A e a são tratados igual
+
+
+Tradução humana da linha inteira
+.replace(/[^a-zà-ú]/gi, '')
+
+
+📌 “Remova todos os caracteres que não sejam letras (inclusive acentuadas), ignorando maiúsculas e minúsculas.”
+
+*/ 
+
+
+
+
+
+let texto = 'Olá mundo!!! 123'
+
+let resultado = texto.replace(/[^a-zà-ú]/gi, '')
+
+console.log(resultado)
