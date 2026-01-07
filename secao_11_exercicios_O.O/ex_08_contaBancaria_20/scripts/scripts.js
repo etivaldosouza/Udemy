26497b253fde017304675ba7d42e20b3fee5bb0c
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-01-07 09:17:53 -0300

    Update scripts.js

--- secao_11_exercicios_O.O/ex_08_contaBancaria_20/scripts/scripts.js
+++ secao_11_exercicios_O.O/ex_08_contaBancaria_20/scripts/scripts.js
@@ -1,5 +1,31 @@
+/*
+    Conta Bancária
+
+Crie uma classe chamada "ContaBancaria" que representa uma conta bancária.
+
+A classe deve ter os seguintes atributos:
+
+numeroConta: representando o número da conta (string ou número).
+
+saldo: representando o saldo da conta (número).
+
+A classe deve ter os seguintes métodos:
+
+depositar(valor): recebe um valor como parâmetro e adiciona esse valor ao saldo da conta.
+
+sacar(valor): recebe um valor como parâmetro e verifica se o saldo é suficiente para efetuar o saque. Se for, realiza o saque subtraindo o valor do saldo. Caso contrário, exibe uma mensagem informando que o saldo é insuficiente.
+
+consultarSaldo(): retorna o saldo atual da conta.
+
+
+
+
+*/ 
+
+
 class ContaBancaria {
     constructor(numeroConta, saldo) {
+        
         if (saldo < 0) {
             throw new Error("Saldo inicial não pode ser negativo");
         }
