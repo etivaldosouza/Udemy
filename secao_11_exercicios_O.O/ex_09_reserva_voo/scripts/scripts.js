57a62184e3094f89e8c3decd57f5b5ebde7d02e7
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-01-07 10:32:27 -0300

    Update scripts.js

--- secao_11_exercicios_O.O/ex_09_reserva_voo/scripts/scripts.js
+++ secao_11_exercicios_O.O/ex_09_reserva_voo/scripts/scripts.js
@@ -28,11 +28,45 @@ class Voo{
     constructor(codigo,origem,destino,qtdeAssentos){
         this.codigo = codigo
         this.origem = origem
+        this.destino = destino
         this.qtdeAssentos = qtdeAssentos
     }
-    reservarAssentos(valor){
-        this.qtdeAssentos = this.qtdeAssentos - 1
+    reservarAssentos(){
+
+        if(this.qtdeAssentos <= 0){
+            console.log('VOO SEM ASSENTOS DISPONÍVEIS')
+            return
+        }
+        this.qtdeAssentos --
+        console.log('Assento reservado com sucesso')
+        
+    }
+    consultarAssentosDisponiveis(){
         return this.qtdeAssentos
     }
 
 }
+
+let voo170 = new Voo(1,'SLZ','REC',100)
+
+let voo377 = new Voo(2,'SLZ','RJ',150)
+
+voo170.reservarAssentos()
+
+voo377.reservarAssentos()
+
+console.log(voo170)
+console.log(voo377)
+
+console.log('qtde de assentos disponiveis voo 170:',voo170.consultarAssentosDisponiveis())
+
+console.log('qtde de assentos disponiveis voo 377:',voo377.consultarAssentosDisponiveis())
+
+voo170.reservarAssentos()
+console.log('qtde de assentos disponiveis voo 170:',voo170.consultarAssentosDisponiveis())
+
+
+
+
+
+
