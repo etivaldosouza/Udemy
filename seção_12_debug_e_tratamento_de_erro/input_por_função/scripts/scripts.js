a60578bbbbd9d0852c3decd6bae2018d7ca3e0eb
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-01-20 18:04:09 -0300

    Update scripts.js

--- seção_12_debug_e_tratamento_de_erro/input_por_função/scripts/scripts.js
+++ seção_12_debug_e_tratamento_de_erro/input_por_função/scripts/scripts.js
@@ -68,18 +68,75 @@ false -> 0
 
 
 function checaNumero(num){
+
     let numero = Number(num)
+
     if(Number.isNaN(numero)){
-        console.log('informe um numero')
-    }else{
-        return numero
+        console.log('ñ é numero')
+        return        
     }
+    
+    console.log(numero)
+      
 }
 
+checaNumero('oi')
+checaNumero('5')
 
-let num = prompt('Digite um válido')
+checaNumero(false)
+checaNumero(true)
 
-checaNumero(num)
 
+/*
+    Explicações:
+
+function checaNumero(num){
+
+    Cria uma função chamada checaNumero que recebe um parâmetro chamado num.
+    Esse parâmetro pode ser qualquer valor passado quando a função for chamada.
+
+
+let numero = Number(num)
+
+    Converte o valor recebido (num) para o tipo Number.
+
+    Se num for algo como "10", vira 10
+
+    Se num for algo como "oi", o resultado será NaN (Not a Number)
+
+    Esse valor convertido é armazenado na variável numero.
+
+
+if(Number.isNaN(numero)){
+
+    Verifica se o valor armazenado em numero é NaN.
+    Number.isNaN() é usado porque ele verifica corretamente se o valor é realmente NaN.
+
+
+
+console.log('ñ é numero')
+    Se o valor não puder ser convertido em número, essa linha é executada e imprime no console:
+
+    ñ é numero
+
+
+ return
+    Encerra a execução da função imediatamente.
+
+    Nada depois dessa linha será executado
+
+    Como não há valor após o return, a função retorna undefined internamente, mas isso não aparece, pois você não está usando console.log fora da função.
+
+
+}
+Fecha o bloco do if.
+
+
+console.log(numero)
+
+    Essa linha só será executada se o valor for um número válido.
+    Se num for "5", por exemplo, será exibido:
+
+*/
 
 
