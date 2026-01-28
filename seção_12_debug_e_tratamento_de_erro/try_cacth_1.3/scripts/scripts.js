5ccd8d18b4a94f3a461cf8638d82e95605c9de8e
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-01-28 15:02:27 -0300

    mais sobre try catch

---
+++ seção_12_debug_e_tratamento_de_erro/try_cacth_1.3/scripts/scripts.js
@@ -0,0 +1,21 @@
+/*
+try{
+
+    inserir a rotina(codigo) passível de erro
+    ñ significa que essa rotina irá gerar um erro
+
+}catch(e){
+
+    recebe o erro(excessao)
+}finally{
+    ñ é obrigatorio e se usado será executado independente de erro ou ñ.
+}
+
+*/ 
+
+
+try{
+
+}catch(e){
+
+}
--- seção_12_debug_e_tratamento_de_erro/try_catch_1.2/scripts/scripts.js
+++ seção_12_debug_e_tratamento_de_erro/try_catch_1.2/scripts/scripts.js
@@ -1,13 +1,13 @@
 function dividir(n1,n2){
-
-    try{
-        let res = n1/n2
-        console.log(res)
-    }catch(e){
-        alert(e.message)      
-    }    
-    alert('teste') // se tiver dentro do finally será executado normalmente
-    
+    let res = n1/n2
+    console.log(res)    
 }
+  
+try{
+    dividir(8,oi)
+}catch(e){
+    alert(e.message)      
+}    
+alert('teste') // se tiver dentro do finally será executado normalmente
 
-dividir(8,neto)
+
