1ecf787c5e190621e742c41b0b3c352306a56598
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-04 09:16:26 -0300

    Update scrips.js

--- seção_13_Expressções_regulares/regex_caracteres_especiais/scrips/scrips.js
+++ seção_13_Expressções_regulares/regex_caracteres_especiais/scrips/scrips.js
@@ -5,4 +5,13 @@
 
 => \w - Um caractere alfanumérico('teste')
 
+=> \s - Qualquer Caractere de espaço em branco
+
+=> \D - Caracteres que não são dígitos
+ 
+=> \W - Caractere não alfanúmerico
+
+=> \S - Caractere que não seja espaço em branco
+
+=> . - Qualquer caractere, menos nova linha
 */ 
