552b0861b76729718f93cba46c79f999b06ab7de
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-08 14:39:18 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/regex_caracteres_especiais_pratica/scripts/scripts.js
+++ seção_13_Expressções_regulares/regex_caracteres_especiais_pratica/scripts/scripts.js
@@ -4,9 +4,11 @@
     
 */
 
-let ano = /\d\d\d\d/
+let ano = /\d\d\d\d/ //verificando se a str tem 4 digitos(se é um ano)
 
 console.log(ano.test('1993'))
+
+console.log(ano.test('19'))
 
 console.log(ano.test('opa'))
 
@@ -18,6 +20,21 @@ console.log(ano.test('#@  '))
 
 //=========================================================//
 
-let palavras = /\w\w\w/
+let palavras = /\w\w\w/   //verificando se a string tem 3 letras
 
 console.log(palavras.test('dia'))
+
+console.log(palavras.test('ano'))
+
+console.log(palavras.test('teste'))
+
+console.log(palavras.test('oi'))
+
+console.log(palavras.test('  '))
+console.log(palavras.test('n '))
+
+
+
+
+
+
