1f8fb2bcd977e1a0b194dc756e94ee45857288c4
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-12 10:45:57 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_exatamente_4_letras/scripts/scripts.js
+++ seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_exatamente_4_letras/scripts/scripts.js
@@ -4,6 +4,9 @@
 
 */ 
 
-let letras = /\w{7}$/
+let letras = /^[A-Za-z]{5}$/    
 
-console.log(letras.test('etivaldo'))
+console.log(letras.test('etiv'))
+
+
+// tem que ter o ^no inicio e o $ no final p/ contar a quantidade certa.
