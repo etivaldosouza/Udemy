00b881bdd47e762786ae078e4b9ef5560076dc22
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-13 08:53:37 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_exatamente_4_letras/scripts/scripts.js
+++ seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_exatamente_4_letras/scripts/scripts.js
@@ -4,12 +4,16 @@
 
 */ 
 
-let letras = /^[A-Za-z]{5}$/    
+let letras = /^[A-Za-z]{4}$/    // comecar e terminar com letras(vai ser exatamente 4 letras e ñ aceita nada após)
 
 console.log(letras.test('etiv'))
 
+console.log(letras.test('1etiv'))
 
-// tem que ter o ^no inicio e o $ no final p/ contar a quantidade certa.
+console.log(letras.test('etiv4'))
+
+
+// tem que ter o ^no inicio e o $ no final p/ contar a quantidade certa. veja o caso abaixo:
 
 
 let tresLetras = /^[A-Za-z]{4}/  // aqui aceita no minimo 4 letras começando com letras
@@ -20,6 +24,7 @@ console.log(tresLetras.test('123Etivaldo'))
 
 console.log(tresLetras.test('Etiv245454'))
 console.log(tresLetras.test('Etiv 245454'))
+console.log(tresLetras.test('Eti 245454'))
 
 
 //============================================//
@@ -42,11 +47,11 @@ console.log(apenasTresLetras.test('rua2'))
 //==============================================================================
 
 
-let apenas8Letras = /[A-Za-z]$/          // tem q terminar apenas com letras
+let soLetras = /[A-Za-z]$/          // tem q terminar apenas com letras
 
-console.log(apenas8Letras.test('123Etivaldo'))
+console.log(soLetras.test('123Etivaldo'))
 
-console.log(apenas8Letras.test('Etivaldo123'))
+console.log(soLetras.test('Etivaldo123'))
 
 //======================================================//
 
@@ -62,3 +67,27 @@ console.log(apenasLetras.test('etivaldo1'))
 console.log(apenasLetras.test('etivald'))
 
 console.log(apenasLetras.test('e'))
+
+
+
+//=====================================================//
+
+// Começando por números:
+
+
+let numeros = /^[0-9]/
+
+console.log(numeros.test('123A'))
+
+console.log(numeros.test('etivaldo123'))
+
+
+//================================================//
+
+// Apenas números
+
+
+let num = /^[0-9]$/ // aqui significa que irá aceitar apenas um número
+
+console.log(num.test('1'))
+
