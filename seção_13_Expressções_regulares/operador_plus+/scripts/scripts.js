76e5302d73e571ab20d1b4893f4b1bdacfddb278
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-13 10:46:56 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/operador_plus+/scripts/scripts.js
+++ seção_13_Expressções_regulares/operador_plus+/scripts/scripts.js
@@ -1,5 +1,13 @@
 /*
     Operador Plus +
 
+=> Quando + está apos alguma expressão, este elemento pode se repetir mais de uma vez
+
 */
 
+
+let muitoOuPoucosDigitos = /\d+/
+
+console.log(muitoOuPoucosDigitos.test('7637634763763746734673267236476347634746736273632763764374647637364764376'))
+
+console.log(muitoOuPoucosDigitos.test(''))
