482adfe2225f0c622734ece260935acc2aa90dc5
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-13 08:58:10 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_apenas_numeros/scripts.js
+++ seção_13_Expressções_regulares/exercicios_ocorrência_precisa/validar_apenas_numeros/scripts.js
@@ -15,8 +15,21 @@
 */ 
 
 
-let num = /\d+$/
+let num = /\d+$/   // um ou mais números seguidos e terminando em numero
 
-console.log(num.test('123'))
+console.log(num.test('1237736347'))
 
 console.log(num.test('123Abcg'))
+
+
+
+//================================================================//
+
+//OBS:
+
+
+let numero = /^[0-9]$/ // aqui significa que irá aceitar apenas um número
+
+console.log(numero.test('1'))
+
+console.log(numero.test('182783787287827'))
