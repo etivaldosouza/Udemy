1c1a41f4acdb68ddc0a0310e87ae559e4efcdc48
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-16 09:47:41 -0300

    metodo match

---
+++ seção_13_Expressções_regulares/metodo_match/scripts/scripts.js
@@ -0,0 +1,19 @@
+/*
+    Método match
+
+=> assim como o exec ele retorna um conjunto com mais informações do que apenas true ou false.quando não encontra retorna null
+
+=> é um método que executamos na string
+
+*/
+
+const frase = 'o número 20 está aqui'.match(/\d+/)
+
+console.log(frase)
+
+
+//===============================================//
+
+const frase2 = 'o númeto está aqui'.match(/\d+/)
+
+console.log(frase2)
