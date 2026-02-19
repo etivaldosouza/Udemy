59e6cc1c2c2f75efe23488b231281ec29db27f54
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-19 19:34:05 -0300

    Update scripts.js

--- seção_13_Expressções_regulares/validando_dominios/scripts/scripts.js
+++ seção_13_Expressções_regulares/validando_dominios/scripts/scripts.js
@@ -6,4 +6,16 @@
 
 */
 
-let validarDominio = /www.\w+\.com|.br|/
+const validarDominio = /^(www.)?\w+.com.br|.com$/;  //$ - determina o final da string
+
+console.log(validarDominio.test("www.kfemprestimos.com.br"))
+
+console.log(validarDominio.test("www.kf"))
+
+console.log(validarDominio.test("www.teste.com"))
+
+console.log(validarDominio.test("www.teste.comor3eiorieorieoi"))
+
+console.log(validarDominio.test("www.266egehw.com"))
+
+console.log(validarDominio.test('kf.com'))
