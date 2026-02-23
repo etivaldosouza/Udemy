cafb0e19c6def734513b31acfb230d7afa6d462a
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-23 20:33:19 -0300

    validacao de email

--- seção_13_Expressções_regulares/validando_dominios/scripts/scripts.js
+++ seção_13_Expressções_regulares/validando_dominios/scripts/scripts.js
@@ -6,7 +6,7 @@
 
 */
 
-const validarDominio = /^(www.)?\w+.com.br|.com$/;  //$ - determina o final da string
+const validarDominio = /^(\www\.)?\w+(\.com\.br|\.com)$/;  //$ - determina o final da string
 
 console.log(validarDominio.test("www.kfemprestimos.com.br"))
 
@@ -21,3 +21,25 @@ console.log(validarDominio.test("www.266egehw.com"))
 console.log(validarDominio.test('kf.com'))
 
 console.log(validarDominio.test('123maria.com'))
+
+
+/*
+^(www.)?\w+.com.br
+
+=> . se deixar sem a barra invertida significa que vai aceitar qualquer caractere
+
+iria aceitar:
+
+xcom
+!com
+@com
+
+(\.com\.br|\.com) → termina com:
+
+.com.br OU
+
+.com
+
+$ → fim da string
+
+*/ 
--- seção_13_Expressções_regulares/validando_email/scripts/scripts.js
+++ seção_13_Expressções_regulares/validando_email/scripts/scripts.js
@@ -5,8 +5,39 @@
 */ 
 
 
-let validarEmail = /w+@\w+.com.br|.com$/
+let validarEmail = /\w+@\w+(\.com.br|\.com)$/
 
 console.log(validarEmail.test('etivaldosouza@hotmail.com'))
 console.log(validarEmail.test('etivaldosouza@hotmail.com.br'))
+console.log(validarEmail.test('123@hotmail.com.br'))
+console.log(validarEmail.test('123_kf@hotmail.com.br'))
+console.log(validarEmail.test('123_kf@hotmail'))
+console.log(validarEmail.test('hotmail.com'))
 
+
+/*
+    OBS:
+
+\w+.com.br
+
+=> . se deixar sem a barra invertida significa que vai aceitar qualquer caractere
+
+iria aceitar:
+
+xcom
+!com
+@com
+
+(\.com\.br|\.com) → termina com:
+
+.com.br OU
+
+.com
+
+$ → fim da string
+
+
+
+*/ 
+
+von
