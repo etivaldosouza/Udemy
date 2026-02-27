92166da4b3a9ea3a3d97e51fa8c6c341a82752a0
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-02-27 09:48:02 -0300

    criado_secao_14_exercicios

---
+++ seção_14_exercicios_regex/ex01_regex_letras_maiusculas/scipts/scripts.js
@@ -0,0 +1,11 @@
+/*
+=> Crie uma regex que aceite apenas letras maiúsculas
+
+=> depois teste
+
+*/ 
+
+let soMaiusc = /S/
+
+console.log(soMaiusc.test('Etivaldo'))
+console.log(soMaiusc.test('ETIVALDO'))
