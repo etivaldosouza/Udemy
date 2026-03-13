95424159144123585257e93b0dfb683be7e342b0
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-03-13 14:45:49 -0300

    Update scripts.js

--- seção_14_exercicios_regex/ex_04_validacao_IP/scripts/scripts.js
+++ seção_14_exercicios_regex/ex_04_validacao_IP/scripts/scripts.js
@@ -5,5 +5,20 @@
 
 => depois teste
 
+
+Para validar um endereço IPv4 (como 127.0.0.1) precisamos lembrar das regras:
+
+Um IP possui 4 números separados por ponto.
+
+Cada número (octeto) vai de 0 até 255.
+
+Exemplo válido: 192.168.1.1
+
+
 */ 
 
+let validaIp = /^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$/
+
+console.log(validaIp.test('45.169.57.87'))
+console.log(validaIp.test("127.0.0"))
+console.log(validaIp.test("192.168.1.1"))
