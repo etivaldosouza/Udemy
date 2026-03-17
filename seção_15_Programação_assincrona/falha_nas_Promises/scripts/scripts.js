6d5515434a226f7384c47ac41d6e6c060264b2c5
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-03-17 15:12:55 -0300

    promise_resolve

---
+++ seção_15_Programação_assincrona/falha_nas_Promises/scripts/scripts.js
@@ -0,0 +1,26 @@
+/*
+    Falhas nas Primises
+
+=>  Uma Promise pode falhar, podemos reter esse erro com um método chamado catch
+
+=> Com ele podemos exibir o erro no console, por exemplo, e fazer o debug no código
+
+*/ 
+
+
+let promiseErrada = Promise.resolve(new Error('Algo deu errado'))
+
+promiseErrada
+    .then((valor) => console.log(valor))
+    .catch(reason => console.log('Erro:' + reason))
+
+
+//=====================================================================//
+
+let p = Promise.resolve(new Error('não deu certo!!'))
+
+console.log('lallalal')
+
+p
+.then(valor => console.log(valor))
+.catch(reason => console.log('Falhou' + reason))
