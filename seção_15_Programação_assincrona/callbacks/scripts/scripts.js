d6dc03927d5f4d4d43c7bc4e89c0b1cd7d7ee0a9
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-03-19 16:35:17 -0300

    adicionado comentario

--- seção_15_Programação_assincrona/callbacks/scripts/scripts.js
+++ seção_15_Programação_assincrona/callbacks/scripts/scripts.js
@@ -36,8 +36,22 @@ console.log('3')
 console.log('4')
 
 /*
-    na programação assincrona vc tem um codigo que é executado fora do fluxo normal do programa ou seja ele é executado num momento futuro como acontece na função setTimeout. vale salientar que no caso da funçao setTimeou ela nao trava o código, ou seja, o programa segue o fluxo normal e após a execução do fluxo normal do programa função setTimout é executada. por isso é assincrona, pois, ela nao trava o programa mas sim é executada em momento futuro após o fluxo normal do programa.
+    na programação assincrona vc tem um código que é executado fora do fluxo normal do programa ou seja ele é executado num momento futuro como acontece na função setTimeout. vale salientar que no caso da funçao setTimeou ela nao trava o código, ou seja, o programa segue o fluxo normal e após a execução do fluxo normal do programa a função setTimout é executada. por isso é assincrona, pois, ela nao trava o programa mas sim é executada em momento futuro após o fluxo normal do programa.
 
     o número 10 é executado de forma assincrona ao fluxo do programa.
 */ 
 
+
+//======================================================================//
+
+console.log('fluxo normal do programa')
+
+setTimeout(function(){
+    console.log('função callback,assincrona.')
+},5000)
+
+function conceito(msg){
+    return msg
+}
+
+console.log(conceito('sou uma função sincrona, seguindo o fluxo normal do programa'))
--- seção_15_Programação_assincrona/promisses/scripts/scripts.js
+++ seção_15_Programação_assincrona/promisses/scripts/scripts.js
@@ -1,7 +1,7 @@
 /*
     Promises
 
-=> As Promisses são ações assíncronas que podem produzir um valor em algum momento no código.
+=> As Promisses também são ações assíncronas que podem produzir um valor em algum momento no código.
 
 => Uma forma de dizer a linguagem que um valor pode estar presente em um futuro do código
 
@@ -24,9 +24,71 @@ console.log('Outros Códigos')
 
 console.log(p)
 
-p.then(function(valor){          // sem aerowfunction
-    return valor + 5
-})
+p.then(valor => valor + 5 )        // sem aerowfunction
+  
+
 .then((valor) => {
     console.log(valor)
 })
+
+
+//============================
+
+
+let nome = Promise.resolve('Etivaldo')
+
+nome.then(function(arg){
+    console.log(`olá ${arg}`)
+})
+
+console.log('este trecho segue o fluxo normal do programa, ou seja sincrono')
+console.log('fim da programação sincrona')
+
+
+//===========================================================================//
+
+/*
+    
+valor => { valor + 5 }
+
+Quando você usa {} em arrow function, precisa usar o nome return explicitamente.
+
+👉 Do jeito que está, a função não retorna nada, então o valor vira undefined.
+
+
+*/
+
+let pi = Promise.resolve(5)
+
+pi.then(valor => {          // usando aerowfunction
+    console.log('Primeiro then recebeu:', valor)
+    valor + 5   // ❌ não retorna nada
+})
+.then(valor => {
+    console.log('Segundo then recebeu:', valor)
+})
+
+//=======================================================//
+
+let pis = Promise.resolve(15)
+
+pis.then(function(valor){
+    console.log('o primeiro then recebeu',valor)
+    return valor + 5         //fica 20 armazenado na memoria
+})
+.then(valor => console.log('Segundo then recebeu:', valor))
+
+    
+
+// vale lembrar que o return armazena o valor na memoria, já o console.log não. so exibe na tela mas nao armazena.
+    
+    
+    
+
+    
+
+    
+pis.then(valor =>{
+    console.log('Segundo then recebeu:', valor)
+})
+    
