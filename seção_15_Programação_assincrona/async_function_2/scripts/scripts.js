cde1f7251ce1f9e593506eb221a44fc574f70989
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-03-25 08:08:30 -0300

    Update scripts.js

--- seção_15_Programação_assincrona/async_function_2/scripts/scripts.js
+++ seção_15_Programação_assincrona/async_function_2/scripts/scripts.js
@@ -14,11 +14,12 @@ async function saudacao(params) {
     return `olá ${params}`
 }
 
+
 saudacao('Etivaldo').then(function(valor){
     console.log(valor)
-})
+}) 
 
-saudacao('Pedro').then(function(valor){
+saudacao('Pedro').then((valor) => {
     console.log(valor)
 })
 
@@ -37,4 +38,12 @@ setTimeout(function(){
     console.log('temos aqui a função setTim e outra função como parâmetro')
 },3000)
 
+function(){ essa função anônima seria um callback
+    console.log('temos aqui a função setTim e outra função como parâmetro')
+}
+
+
+relembrando: 
+
+=>  a função setTimeout é uma função asssyncrona e ela recebe dois parâmetros: um é uma função anônima e o outro é o tempo em que ela vai demorar para ser executada
 */ 
