ee9a60dc0d7a8948386f170b9d320812c43bfa25
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-01 15:48:34 -0300

    encontrado por class

---
+++ javascripts_no_navegador/queryselector/scripts/scripts.js
@@ -0,0 +1,19 @@
+/*
+    Encontrando elementos: por class
+
+=> Utilizamos o seguinte método para encontrar elementos por class:
+
+=> Document.getElementsByClassname('alguma-classe');
+
+
+*/ 
+
+
+console.log(document.getElementsByTagName('li'))
+
+console.log(document.getElementsByClassName('items-azuis'))
+
+
+
+
+
