d939c75796c9da219d553ed85a4764c4b95fc03f
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-01 16:21:13 -0300

    alterando elementos

---
+++ javascripts_no_navegador/alterando_elemento/scripts/scripts.js
@@ -0,0 +1,16 @@
+/*
+    Alterando o HTML
+
+=> Podemos mudar quase tudo através do DOM;
+
+=> Adicionar,remover e clonar elementos;
+
+=> Podemos utilizar métodos como: insertBefore,appendChild e replaceChild;
+
+
+=> Vamos ver em detalhes como utilizar cada um destes;
+
+*/ 
+
+
+
