df989c2a3c7304afc1788422534e3fb864551d48
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-01 09:59:35 -0300

    criado pasta DOM

---
+++ javascripts_no_navegador/Dom/scripts/scripts.js
@@ -0,0 +1,30 @@
+/*
+    A Estrutura  HTML e o DOM
+
+=> Quando uma página web é requisitada, ela recebe um texto que é transformado em uma estrutura HTML;
+
+=> As Tags vão se aninhando uma às outras, formando uma estrutura em que elementos ficam dentro dos outros;
+
+=> Essa estrutura tem uma representação exatamente igual para o JS chamada DOM
+
+=> Que significa Document Object Model;
+
+=> E por meio do JS poidemos acessar cada um destes elementos do HTML através do DOM.
+
+estrutura aninhada no html:
+
+exemplo:
+
+<div> pai
+    <h1> Título 1</h1>filho
+    <p> Algum texo </p>filho
+</div>
+
+*/ 
+
+
+/*
+    o dom seria um estrutura de arvore com nós semelhante a do HTML onde através dessa
+    estrutura podemos fazer alterações no HTML.
+
+*/
