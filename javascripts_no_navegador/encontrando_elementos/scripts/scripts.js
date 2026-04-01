951aa2efe8b2fe7ced514c2d489425bdb9ae9a1f
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-01 14:31:51 -0300

    elementos por tag

---
+++ javascripts_no_navegador/encontrando_elementos/scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    Encontrando elementos
+
+=> Podemos encontrar elementos de uma forma mais fácil do que se movendo pelo DOM
+
+=> Como no HTML temos tags, classes e ids, o javaScript nos possibilita utilizar estas características para encontrar o que desejamos;
+
+=> Há alguns métodos para isto: getElementsByTagName,getElementById, getElemtsByClassName e também o querySelector
+
+*/ 
+
+
---
+++ javascripts_no_navegador/encontrando_por_tag/scripts/scripts.js
@@ -0,0 +1,15 @@
+/*
+    Encontrando elementos: por tag
+
+=> Utilizamos o seguinte método para encontrar elementos pela tag.
+
+=> Document.getElementByTagName('h1');
+
+
+*/ 
+
+
+console.log(document.getElementsByTagName('h1'))
+
+
+console.log(document.getElementsByTagName('p'))
