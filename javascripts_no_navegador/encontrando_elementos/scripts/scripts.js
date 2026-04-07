ee59ba6d8653dc57def46a0540db73c6838b5bdd
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-07 12:05:20 -0300

    adicionado comentario

--- javascripts_no_navegador/encontrando_elementos/scripts/scripts.js
+++ javascripts_no_navegador/encontrando_elementos/scripts/scripts.js
@@ -3,7 +3,7 @@
 
 => Podemos encontrar elementos de uma forma mais fácil do que se movendo pelo DOM
 
-=> Como no HTML temos tags, classes e ids, o javaScript nos possibilita utilizar estas características para encontrar o que desejamos;
+=> Como no HTML temos tags, classes e id's, o javaScript nos possibilita utilizar estas características para encontrar o que desejamos;
 
 => Há alguns métodos para isto: getElementsByTagName,getElementById, getElemtsByClassName e também o querySelector
 
--- javascripts_no_navegador/encontrando_por_class/scripts/scripts.js
+++ javascripts_no_navegador/encontrando_por_class/scripts/scripts.js
@@ -11,9 +11,13 @@
 
 console.log(document.getElementsByTagName('li'))
 
-console.log(document.getElementsByClassName('items-azuis'))
+console.log(document.getElementsByClassName('itens-azuis'))
+
+console.log(document.getElementsByClassName('itens-vermelhos'))
 
 
+
+console.log(document.getElementsByClassName('itens'))
 
 
 
