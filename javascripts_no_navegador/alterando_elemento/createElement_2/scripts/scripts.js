18cc576c66104c829f0126ff02949fa7552225ad
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-14 20:35:30 -0300

    creatElement

---
+++ javascripts_no_navegador/alterando_elemento/createElement_2/scripts/scripts.js
@@ -0,0 +1,23 @@
+/*
+    Criando Elemento:
+
+=>  Podemos criar um nó de elemento com o creatElemet, e depois inserir no HTML.
+
+*/ 
+
+let lista = document.createElement('ul')
+
+for(i = 0; i < 5; i++){
+    let item = document.createElement('li')
+    let texto = document.createTextNode(`texto lista ${i}`)
+    item.appendChild(texto)
+    lista.appendChild(item)
+}
+
+
+let posicaoLista = document.querySelector('#container-principal')
+
+posicaoLista.appendChild(lista)
+
+
+
