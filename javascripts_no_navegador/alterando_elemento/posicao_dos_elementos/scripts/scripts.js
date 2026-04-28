cf915ea26054b51a69d12d4b4762a25bda08e264
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-04-28 17:00:28 -0300

    Update scripts.js

--- javascripts_no_navegador/alterando_elemento/posicao_dos_elementos/scripts/scripts.js
+++ javascripts_no_navegador/alterando_elemento/posicao_dos_elementos/scripts/scripts.js
@@ -1,27 +1,15 @@
 /*
-    Verificando altura e largura do elemento:
+    Posição do Elemento na tela:
 
-=>  Com o JS também é possível verificar propriedades do elemento como altura e largura, veja:
+=>  Também é possível checar a posição do elemento na tela
 
-
-obs: 
-
-.offsetWidth => considera a borda do elemento
-.offsetHeight => considera a borda do elemento
-
-
-.clientWidth => desconsidera borda
-.clientHeight => desconsidera borda
+=> getBoundingClientRect nos da as posições de top,left,rigth,bottom do elemento e outras informações;
 
 */ 
 
 
 let elemento = document.querySelector('#titulo-principal')
 
-console.log('Largura: '+ elemento.offsetWidth) // verificar a largura do elemento considerando borda
-console.log('Altura: '+ elemento.offsetHeight) // verifica a altura do elemento considerando borda
-
-console.log('Largura: '+elemento.clientWidth)
-console.log('Largura: '+elemento.clientHeight)
+console.log(elemento.getBoundingClientRect())
 
 
