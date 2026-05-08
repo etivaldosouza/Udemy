aef2a334986e2d4a5b6f5d814776d228ebbf99e2
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-05-08 14:27:59 -0300

    evento na pratica

---
+++ seção_17_eventos_com_javaScript/conceitos/scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    O que são eventos:
+
+=> Ações condicionadas a algum tipo de resposta feita pelo usuário;
+
+=> Por exemplo: clicks do mouse, apertar alguma tecla ou até movimento do mouse;
+
+=> Podemos atrelar lógica a essas ações do usuário;
+
+=> Por meio de handlers(funcoes relacionadas a eventos);
+
+*/ 
---
+++ seção_17_eventos_com_javaScript/criando_eventos/scripts/scripts.js
@@ -0,0 +1,27 @@
+/*
+    Como acionar um evento:
+
+=> Devemos atrelar o evento a um elemento,por exemplo um botão;
+
+=> Depois inserir um listener e o tipo de evento como argumento;
+
+=> Aí o elemento responderá por este evento e, obviamente, os outros da página não;
+
+
+*/ 
+
+
+let but = document.querySelector('button')
+
+but.addEventListener('mouseover',function(){
+    but.style.backgroundColor = 'red'
+    
+
+})
+
+but.addEventListener('click',()=>{     // aerowfunction
+    but.style.backgroundColor = ''
+    alert('vc clicou')
+})
+
+
