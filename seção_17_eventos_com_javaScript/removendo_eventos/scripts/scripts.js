87e72fa79bd41176f4664b61ea18cabb357f7ae6
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-05-11 16:17:29 -0300

    Update scripts.js

--- seção_17_eventos_com_javaScript/removendo_eventos/scripts/scripts.js
+++ seção_17_eventos_com_javaScript/removendo_eventos/scripts/scripts.js
@@ -20,3 +20,18 @@ setTimeout(function(){
     but.removeEventListener('click',msg)
 },3000)
 
+/*
+
+quando estamos usando o remove somos obrigados a usar uma funcao externa. que foi o caso da function msg.
+
+Quando você usa addEventListener, o JavaScript precisa “guardar” qual função foi adicionada para depois conseguir remover ela.
+
+O JS guarda a referência da função msg.
+
+E depois conseguimos remover:
+
+but.removeEventListener('click', msg)
+
+Agora ele sabe exatamente qual função apagar.
+
+*/ 
