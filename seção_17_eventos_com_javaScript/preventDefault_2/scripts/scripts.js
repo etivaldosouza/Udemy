0865b1131d3aa7d4849e44cc932151065c29e267
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-05-13 08:07:12 -0300

    Update scripts.js

--- seção_17_eventos_com_javaScript/preventDefault_2/scripts/scripts.js
+++ seção_17_eventos_com_javaScript/preventDefault_2/scripts/scripts.js
@@ -3,6 +3,8 @@ const tarefaInp = document.getElementById('#tarefaInput')
 
 
 function executaEvento(e){
-    console.log(`tipo do evento`) 
+    console.log(`tipo do evento: ${e.type}`) 
+    console.log(tarefaInput.value)
+    e.preventDefault()
 }
-form.addEventListener('submit',executaEvento)
+form.addEventListener('submit',executaEvento)   
