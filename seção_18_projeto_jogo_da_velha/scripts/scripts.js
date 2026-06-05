d33cc5cea563651338b580919d015b3e5ce28872
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-06-05 15:12:28 -0300

    javascript do projeto

---
+++ seção_18_projeto_jogo_da_velha/scripts/scripts.js
@@ -0,0 +1,39 @@
+let x = document.querySelector('.x')
+let o = document.querySelector('.o')
+let boxes =  document.querySelector('.box')
+let buttons =  document.querySelector('#buttons-container button')
+let messageContainer =  document.querySelector('#message')
+let messageText =  document.querySelector('#message p')
+let secondPlayer;
+
+
+/* contador de jogadas */ 
+
+let player1 = 0
+let player2 = 0
+
+
+/* adicionando evento de click aos boxes */ 
+
+
+
+for(let i = 0; i < boxes.length; i++){
+
+    // quando alguem clica na caixa
+
+    boxes[i].addEventListener('click',function(){
+
+        let el;
+        
+        if(player1 == player2){
+            // x
+            el = x
+        }else{
+            // o
+            el = o
+        }
+
+        let cloneEl = el.cloneNode(true)
+        this.appendChild(cloneEl)
+    })
+}
