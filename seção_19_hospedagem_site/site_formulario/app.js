b68bcb69af8bac432fcf7a6e550805bc92468bea
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-06-26 17:35:31 -0300

    finalizando css

--- seção_19_hospedagem_site/site_formulario/app.js
+++ seção_19_hospedagem_site/site_formulario/app.js
@@ -1,1 +1,25 @@
-xsad
+// selecionar elemento
+
+const root = document.documentElement;
+const btn = document.getElementById('btnTema');
+const ano = document.getElementById('ano')
+
+
+//preenchendo ano atual:
+ano.textContent = new Date().getFullYear()
+
+// tema ligth/dark
+
+btn.addEventListener('click',function(){
+    const escuro = root.getAttribute('data-tema') === 'escuro'
+
+    if(escuro){
+        root.removeAttribute('data-tema')
+        localStorage.setItem('tema', 'claro')
+    }else{
+        root.setAttribute('data-tema','escuro')
+        localStorage.setItem('tema', 'escuro')
+    }
+})
+
+
