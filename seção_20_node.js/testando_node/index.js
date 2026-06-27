1d54c4c7f7dad40b9f4f7d75ea68815d95e96d64
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-06-26 21:35:23 -0300

    Create index.js

---
+++ seção_20_node.js/testando_node/index.js
@@ -0,0 +1,28 @@
+/*
+    Nodes.js
+
+=>  Não é uma linguagem de programação
+
+=> Uma ferramenta construida sobre o motor javaScript do google chrome para criar aplicações rápidas e escaláveis;
+
+=> O problema: cada conexão de um cliente aloca um espaço na memória;
+
+=> Com Node: cada conexão é um evento executado na engine no node.
+
+=> Ou seja, o Node suporta muito mais conexões do que PHP,JAVA em uma máquina igual, por exemplo;
+
+
+VANTAGENS DO NODE.JS:
+
+
+=> Muito leve, pouco uso da memória ram, melhor aproveitamento da cpu;
+
+=> utiliza JavaScript;
+
+=> Tem um dos maiores ecosistemas de bibliotecas, módulos e plug-ins do mundo.
+
+*/ 
+
+
+let msg = 'Hellow world'
+console.log(msg)
