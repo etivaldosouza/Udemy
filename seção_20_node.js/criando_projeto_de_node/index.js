c7691023ef3e51397616e6dd8988b952976b76ae
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-08-05 08:25:10 -0300

    excluido pasta

--- seção_20_node.js/criando_projeto_de_node/index.js
+++ seção_20_node.js/criando_projeto_de_node/index.js
@@ -14,3 +14,26 @@
 => package.json(arquivo de configuração do projeto)
 
 */ 
+
+
+let express = require('express')
+
+let app = express()
+
+// criando uma rota de exemplo
+app.get('/',function(req,res){
+    res.send('Primeira rota com Express')
+})
+
+app.get('/teste',function(req,res){
+    res.send('Testando Rota')
+})
+
+app.get('/teste2',function(req,res){
+    res.send('Testando mais uma Rota')
+})
+
+
+app.listen(3000,function(){
+    console.log('A aplicação está funcionando na porta 3000')
+})
