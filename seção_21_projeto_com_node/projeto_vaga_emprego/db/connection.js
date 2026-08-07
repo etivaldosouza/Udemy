6f19424d3fe852221b52542ad9af936805cdac05
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-08-07 10:33:07 -0300

    criado banco

--- seção_21_projeto_com_node/projeto_vaga_emprego/db/connection.js
+++ seção_21_projeto_com_node/projeto_vaga_emprego/db/connection.js
@@ -2,8 +2,7 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './db/app.db'
+    storage: './db/app.db' // estou dizendo que vou criar o arquivo app.db na pasta db
 })
 
 module.exports = sequelize
-
