98f872200464d9f2fe8de0f0d3cf4fbd9623ee7c
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-08-07 09:27:36 -0300

    criado bando dados do projeto

--- seção_21_projeto_com_node/projeto_vaga_emprego/app.js
+++ seção_21_projeto_com_node/projeto_vaga_emprego/app.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const db = require("./db/connection");
+const PORT = 3000;
 
-const PORT = 3000
+// rota
+app.get("/", (req, res) => {
+    res.send("Está funcionando 3");
+});
 
-app.get('/',(req,res) => {
-    res.send('Está funcionando 3')
-})
+app.listen(PORT, function () {
+    console.log(`O express está rodando na porta ${PORT}`);
+});
 
-app.listen(PORT, function(){
-    console.log((`O express está rodando na porta ${PORT}`))
-})
+// conexao do banco
 
+db.authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch((err) => {
+        console.log("Ocorreu um erro ao conectar", err);
+    });
 
 // o nodemon serve para atualizar a aplicação sem precisar reiniciar o servidor;
-
