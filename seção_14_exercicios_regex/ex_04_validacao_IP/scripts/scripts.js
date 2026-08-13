a4908caeab0c4ddd4971c1d8812c807834dcdd68
etivaldosouza <etivaldosouza@hotmail.com>
2026-08-13 09:27:47 -0300

    Update scripts.js

--- seção_14_exercicios_regex/ex_04_validacao_IP/scripts/scripts.js
+++ seção_14_exercicios_regex/ex_04_validacao_IP/scripts/scripts.js
@@ -6,3 +6,11 @@
 => depois teste
 
 */ 
+
+let validaIp = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
+
+console.log(validaIp.test('1.0.0.4'))
+console.log(validaIp.test('112.0.0.4'))
+console.log(validaIp.test("8.8.8.8"))
+console.log(validaIp.test("193.5.4.1.3"))
+console.log(validaIp.test("1934.5.4.1"))
