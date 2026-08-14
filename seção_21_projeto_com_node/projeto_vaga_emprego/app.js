9f74da175621814d4f8e689aebe78e0c0b446ebf
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-08-14 16:45:01 -0300

    adicionando o handlebars

--- seção_21_projeto_com_node/projeto_vaga_emprego/app.js
+++ seção_21_projeto_com_node/projeto_vaga_emprego/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const exphbs = require('express-handlebars')
 const app = express();
+const path = require('path')
 const db = require("./db/connection");
 const PORT = 3000;
-const bodyParsers = require('body-parser')
+const bodyParsers = require('body-parser');
+const { ExpressHandlebars } = require("express-handlebars");
 
 // body parsers
 app.use(bodyParsers.json())
 app.use(bodyParsers.urlencoded({ extended: false }));
+
+
+// handle bars
+app.set('views',path.join(__dirname,'views')) //onde vai ficar os tamplat do projeto
+app.engine('handlebars', exphbs({defaultLayout:'main'})) //arquivo principal de layout
+app.set('view engine', 'handlebars') // qual biblioteca vai utilizar a views
+
+
+// static folder
+app.use(express.static(path.join(__dirname,'public'))) // qual é a pasta de arquivos estaticos
+
 
 // routes
 app.get("/", (req, res) => {
