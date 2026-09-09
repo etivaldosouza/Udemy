2dba9b72214e16357436ef70b238424f5eaf2852
EtivaldoSouza <etivaldosouza@hotmail.com>
2026-09-09 14:40:59 -0300

    escrevendo a lógica

--- seção_22_quiz_com_vanila/projeto_quiz/js/scripts.js
+++ seção_22_quiz_com_vanila/projeto_quiz/js/scripts.js
@@ -1,1 +1,85 @@
-console.log('teste')
+// Declaração de variável
+
+const question = document.querySelector('#question')
+const ansersBox = document.querySelector('#answers-box')
+const quizzContainer = document.querySelector('#quizz-container')
+const scoreContainer = document.querySelector('#score-container')
+const lettrs = ['a','b','c','d']
+let poinsts = 0;
+let actualQuestion = 0
+
+// Perguntas
+
+const questions = [
+  {
+    "question": "PHP foi desenvolvido para qual fim?",
+    "answers": [
+      {
+        "answer": "back-end",
+        "correct": true
+      },
+      {
+        "answer": "front-end",
+        "correct": false
+      },
+      {
+        "answer": "Sistema operacional",
+        "correct": false
+      },
+      {
+        "answer": "Banco de dados",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Uma forma de declarar variável em JavaScript:",
+    "answers": [
+      {
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
+        "correct": true
+      },
+      {
+        "answer": "@var",
+        "correct": false
+      },
+      {
+        "answer": "#let",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Qual o seletor de id no CSS?",
+    "answers": [
+      {
+        "answer": "#",
+        "correct": true
+      },
+      {
+        "answer": ".",
+        "correct": false
+      },
+      {
+        "answer": "@",
+        "correct": false
+      },
+      {
+        "answer": "/",
+        "correct": false
+      },
+    ]
+  },
+]
+
+// Substituição do quizz para a primeira pergunta
+
+function init(){
+    console.log('Iniciou!')
+}
+
+init()
